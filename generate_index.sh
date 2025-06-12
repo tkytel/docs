64d57166187654952fb58fc167565183a7d1eda1
ねじくぎ/nejikugi <63852665+scrwnl@users.noreply.github.com>
2025-06-12 13:39:23 +0000

    色々

--- generate_index.sh
+++ generate_index.sh
@@ -18,11 +18,12 @@ escape() {
 }
 
 # 最悪
-echo "<!DOCTYPE html>
-<html lang=\"ja\">
+cat <<EOF
+<!DOCTYPE html>
+<html lang="ja">
 <head>
-<meta charset=\"UTF-8\">
-<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>TKYTEL COMMENT</title>
 </head>
 <body>
@@ -31,33 +32,38 @@ echo "<!DOCTYPE html>
 <table>
 <thead>
 <tr>
-<th scope=\"col\">No</th>
-<th scope=\"col\">Content</th>
-<th scope=\"col\">Date</th>
+<th scope="col">No</th>
+<th scope="col">Content</th>
+<th scope="col">Date</th>
 </tr>
 </thead>
-<tbody>"
+<tbody>
+EOF
 
 for filename in *.txt;do
     # 文書番号
     NUMBER=${filename%.txt}
     # 著者名
-    AUTHOR=`sed -n 2p ${filename} | sed "s/^ *//" | escape`
+    AUTHOR=$(sed -n 2p ${filename} | sed "s/^ *//" | escape)
     # 表題を空行まで取得し、先頭の空白・改行を削除
-    TITLE=`sed -n "5,/^$/p" ${filename} | sed "s/^ *//" | sed -z "s/\\n//g" | escape`
+    TITLE=$(sed -n "5,/^$/p" ${filename} | sed "s/^ *//" | sed -z "s/\\n//g" | escape)
     
-    DATE_RAW=`sed -n 3p ${filename} | sed "s/^ *//" | escape`
-    DATE_SLASHED=`sed -n 3p ${filename} | xargs date "+%Y/%m/%d" --date | escape`
+    DATE_RAW=$(sed -n 3p ${filename} | sed "s/^ *//" | escape)
+    DATE_SLASHED=$(sed -n 3p ${filename} | sed "s/^ *//" | tr -- - / | escape)
 
-    echo "<tr>
-<th scope=\"row\"><a href=\"${filename}\">TC${NUMBER}</a></th>
+    cat <<EOF
+<tr>
+<th scope="row"><a href="${filename}">TC${NUMBER}</a></th>
 <td>${TITLE}</td>
-<td><time datetime=\"${DATE_RAW}\">${DATE_SLASHED}</time></td>
-</tr>"
+<td><time datetime="${DATE_RAW}">${DATE_SLASHED}</time></td>
+</tr>
+EOF
 done
 
 # 最悪2
-echo "</tbody>
+cat <<EOF
+</tbody>
 </table>
 </body>
-</html>"
+</html>
+EOF
