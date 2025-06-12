687107a4fbc9024b8f1a40a5694588ff62e8b7ff
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2025-06-12 18:56:45 +0900

    えっちてぃーえむえるえすけーぷ

--- generate_index.sh
+++ generate_index.sh
@@ -5,6 +5,18 @@ set -eu
 #区切り文字の置換？を無効化
 IFS=""
 
+# 簡易 HTML エスケープ
+escape() {
+	sed -e "
+		s/&/\\&amp;/g;
+		s/'/\\&#x27;/g;
+		s/\`/\\&#x60;/g;
+		s/\"/\\&quot;/g;
+		s/</\\&lt;/g;
+		s/>/\\&gt;/g;
+	"
+}
+
 # 最悪
 echo "<!DOCTYPE html>
 <html lang=\"ja\">
@@ -30,12 +42,12 @@ for filename in *.txt;do
     # 文書番号
     NUMBER=${filename%.txt}
     # 著者名
-    AUTHOR=`sed -n 2p ${filename} | sed "s/^ *//"`
+    AUTHOR=`sed -n 2p ${filename} | sed "s/^ *//" | escape`
     # 表題を空行まで取得し、先頭の空白・改行を削除
-    TITLE=`sed -n "5,/^$/p" ${filename} | sed "s/^ *//" | sed -z "s/\\n//g"`
+    TITLE=`sed -n "5,/^$/p" ${filename} | sed "s/^ *//" | sed -z "s/\\n//g" | escape`
     
-    DATE_RAW=`sed -n 3p ${filename} | sed "s/^ *//"`
-    DATE_SLASHED=`sed -n 3p ${filename} | xargs date "+%Y/%m/%d" --date`
+    DATE_RAW=`sed -n 3p ${filename} | sed "s/^ *//" | escape`
+    DATE_SLASHED=`sed -n 3p ${filename} | xargs date "+%Y/%m/%d" --date | escape`
 
     echo "<tr>
 <th scope=\"row\"><a href=\"${filename}\">TC${NUMBER}</a></th>
